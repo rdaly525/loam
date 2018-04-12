38684a1606c51c06abca6a58da0c9beb044f0fae
Lenny Truong <lenny@stanford.edu>
2018-04-12 14:30:36 -0700

    Migrate requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
 
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=False)
-
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='loam',
     version='0.1-alpha',
     description='Model boards and parts using Magma',
     packages=["loam"],
-    
-    install_requires=reqs
+    install_requires=[
+        "six"
+    ],
+    url='https://github.com/phanrahan/mantle',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    python_requires='>=3.6'
     )
